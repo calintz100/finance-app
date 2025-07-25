ab9c553781d15c6519166bb80cb44a98040e400e
shin <calintz100@gmail.com>
2025-07-25 17:05:37 +0900

    Add about route

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome2');
+Route::get('/about', function () {
+    return view('about');
 });
